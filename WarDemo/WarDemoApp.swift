f0bd65b1dff09fbd9579329f4f5ffdb14a1f7097
cyu72 <cyu72@binghamton.edu>
2022-11-04 10:53:24 -0400

    first commit

---
+++ WarDemo/WarDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarDemoApp.swift
+//  WarDemo
+//
+//  Created by Chris Yu on 11/4/22.
+//
+
+import SwiftUI
+
+@main
+struct WarDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
